d91429b357b106f7b4775df6b4f40a5e183fab71
project-AFRO <78296190+project-AFRO@users.noreply.github.com>
2022-05-23 00:29:20 +0200

    initalising contact list view

---
+++ Contacts/ContactItem.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace ChatApplication.Contacts
+{
+    partial class ContactItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.profilePicture = new ChatApplication.CircularPictureBox();
+            this.contactName = new System.Windows.Forms.Label();
+            this.lastSeen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.profilePicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // profilePicture
+            // 
+            this.profilePicture.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.profilePicture.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.profilePicture.BorderColor2 = System.Drawing.Color.HotPink;
+            this.profilePicture.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.profilePicture.BorderSize = 2;
+            this.profilePicture.GradientAngle = 50F;
+            this.profilePicture.Location = new System.Drawing.Point(2, 2);
+            this.profilePicture.Margin = new System.Windows.Forms.Padding(2);
+            this.profilePicture.Name = "profilePicture";
+            this.profilePicture.Size = new System.Drawing.Size(114, 114);
+            this.profilePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.profilePicture.TabIndex = 1;
+            this.profilePicture.TabStop = false;
+            // 
+            // contactName
+            // 
+            this.contactName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contactName.Location = new System.Drawing.Point(121, 35);
+            this.contactName.Name = "contactName";
+            this.contactName.Size = new System.Drawing.Size(411, 23);
+            this.contactName.TabIndex = 2;
+            this.contactName.Text = "label1";
+            // 
+            // lastSeen
+            // 
+            this.lastSeen.Location = new System.Drawing.Point(121, 72);
+            this.lastSeen.Name = "lastSeen";
+            this.lastSeen.Size = new System.Drawing.Size(156, 18);
+            this.lastSeen.TabIndex = 3;
+            this.lastSeen.Text = "label2";
+            // 
+            // ContactItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.contactName);
+            this.Controls.Add(this.lastSeen);
+            this.Controls.Add(this.profilePicture);
+            this.Name = "ContactItem";
+            this.Size = new System.Drawing.Size(535, 118);
+            ((System.ComponentModel.ISupportInitialize)(this.profilePicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CircularPictureBox profilePicture;
+        private System.Windows.Forms.Label contactName;
+        private System.Windows.Forms.Label lastSeen;
+    }
+}
---
+++ Contacts/ContactItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatApplication.Contacts
+{
+    public partial class ContactItem : UserControl
+    {
+        public ContactItem()
+        {
+            InitializeComponent();
+        }
+
+        #region Properties
+
+        private string _imgUrl;
+        private string _name;
+        private string _lastSeen;
+
+        [Category("Contacts Card")]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        [Category("Contacts Card")]
+        public string LastSeen
+        {
+            get { return _lastSeen; }
+            set { _lastSeen = value; }
+        }
+
+        [Category("Contacts Card")]
+        public string ImgUrl
+        {
+            get { return _imgUrl; }
+            set { _imgUrl = value; }
+        }
+        #endregion
+
+    }
+}
